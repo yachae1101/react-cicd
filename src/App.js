3a8d18e2bc91895c841ec6783cbe83298d824072
yachae1101 <yachae1101@naver.com>
2024-11-25 16:35:32 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>1. App.js 수정</h2>
+      <h2>2. github push</h2>
+      <h2>3. github action 에 의해</h2>
+      <h2>4. S3에 배포</h2>
     </div>
   );
 }
